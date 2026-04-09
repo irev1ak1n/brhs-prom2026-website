cda2bb2996827c85647ffb306a952ca7e45b6a46
Illia Reviakin <illiareviakin2008@gmail.com>
2026-04-09 02:04:53 -0400

    improved images sizes

--- resources/views/pages/gallery.blade.php
+++ resources/views/pages/gallery.blade.php
@@ -23,43 +23,43 @@
     <section class="gallery-page">
         <div class="container">
             <div class="gallery-mosaic">
-                <article class="gallery-item span-lg">
+                <article class="gallery-item feature">
                     <img src="{{ asset('images/gallery/place1.jpg') }}" alt="Prom venue atmosphere">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/building.png') }}" alt="Prom venue exterior">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item no-crop poster">
                     <img src="{{ asset('images/gallery/prom2026.png') }}" alt="Prom 2026 detail">
                 </article>
 
-                <article class="gallery-item tall">
+                <article class="gallery-item tall-xl">
                     <img src="{{ asset('images/gallery/event2.png') }}" alt="Interior event space">
                 </article>
 
-                <article class="gallery-item wide">
+                <article class="gallery-item wide-md">
                     <img src="{{ asset('images/gallery/event3.png') }}" alt="Dance floor area">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/overview1.png') }}" alt="Room overview">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/tickets.png') }}" alt="Prom tickets">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item sm">
                     <img src="{{ asset('images/gallery/cookies.png') }}" alt="Cookies for prom">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item sm">
                     <img src="{{ asset('images/gallery/cakes.png') }}" alt="Desserts for prom">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item sm">
                     <img src="{{ asset('images/gallery/pizza.png') }}" alt="Pizza for prom">
                 </article>
 
@@ -67,7 +67,7 @@
                     <img src="{{ asset('images/gallery/food.png') }}" alt="Prom food table">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/food2.png') }}" alt="Food and refreshments">
                 </article>
 
@@ -75,31 +75,31 @@
                     <img src="{{ asset('images/gallery/food3.png') }}" alt="Prom menu items">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/karaoke.png') }}" alt="Karaoke area">
                 </article>
 
-                <article class="gallery-item wide">
+                <article class="gallery-item wide-lg">
                     <img src="{{ asset('images/gallery/karaoke2.png') }}" alt="Seating and activity area">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/karaoke3.png') }}" alt="Stage and lighting area">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item md">
                     <img src="{{ asset('images/gallery/karaoke4.png') }}" alt="Interior lounge view">
                 </article>
 
-                <article class="gallery-item tall">
+                <article class="gallery-item tall-xl">
                     <img src="{{ asset('images/gallery/karaoke5.png') }}" alt="Entertainment room">
                 </article>
 
-                <article class="gallery-item">
+                <article class="gallery-item no-crop poster">
                     <img src="{{ asset('images/gallery/will-you.png') }}" alt="Prom detail graphic">
                 </article>
 
-                <article class="gallery-item wide">
+                <article class="gallery-item no-crop poster">
                     <img src="{{ asset('images/gallery/breaking-news.png') }}" alt="Prom detail announcement">
                 </article>
             </div>
